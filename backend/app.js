c1497d849b17118f47c5ee3df6df98a413a2fef3
Aurélie Mlynarz <aureliemlynarz@gmail.com>
2020-09-07 10:16:56 +0200

    creation fichiers backend

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const salsaRoutes = require('./routes/salsa');
+const userRoutes = require('./routes/user');
+
+mongoose.connect('***',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+app.use('/api/salsa', salsaRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
