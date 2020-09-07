832f4ca3ff5a69d4e3847234ace807d97a3f0475
Aurélie Mlynarz <aureliemlynarz@gmail.com>
2020-09-07 13:06:39 +0200

    création routes

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 
-const salsaRoutes = require('./routes/salsa');
+const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
-mongoose.connect('***',
+mongoose.connect('mongodb+srv://lilimly:yipi2616@cluster0.3qkhx.mongodb.net/piquante?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use('/api/salsa', salsaRoutes);
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
